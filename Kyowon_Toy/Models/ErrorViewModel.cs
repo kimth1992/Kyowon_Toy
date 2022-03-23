b48a2f8a92ff7d32db7e1094e4df6bfaf8dfd30a
kimth1992 <kimth1992@naver.com>
2022-03-23 10:31:23 +0900

    초기 커밋.

---
+++ Kyowon_Toy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kyowon_Toy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
