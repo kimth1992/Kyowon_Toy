00fe13935beda4362fe616aec451b87056244d66
kimth1992 <kimth1992@naver.com>
2022-03-25 16:52:09 +0900

    add project

---
+++ Kyowon_Toy/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Kyowon_Toy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kyowon_Toy.Controllers
+{
+    public class LoginController : Controller
+    {
+
+
+        public LoginController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return Redirect("/login/login");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
+      
---
+++ Kyowon_Toy/Models/Login/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Kyowon_Toy.Models.Login
+{
+    public class UserModel
+    {
+        public int User_seq { get; set; }
+        public string User_name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+
+
+
+    }
+}
